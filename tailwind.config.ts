ebd913156d23d5d83e17959300df1d95054ca891
DOEABLE <kimdh090588@gmail.com>
2024-10-21 17:57:38 +0900

    [feat] button component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         customGreen: '#069894',
+        customWhite: '#EFF0F4',
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
