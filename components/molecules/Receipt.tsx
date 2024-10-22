3272c44d21c2a6adeb07f90433d7b3e6794772db
Sung Yeob Jung <sung87331@gmail.com>
2024-10-22 15:02:10 +0900

    [feat] start Receipt

---
+++ components/molecules/Receipt.tsx
@@ -0,0 +1,13 @@
+import ColorChip from '../atoms/color_chips';
+
+export default function Receipt() {
+  return (
+    <>
+      <div className='border p-5'>
+        <div>
+          <ColorChip color='blue'>예금</ColorChip>
+        </div>
+      </div>
+    </>
+  );
+}
