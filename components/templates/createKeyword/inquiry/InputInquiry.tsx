213363774c907e393abb3cd0247bb0f3cd567261
insun-k <kis8294@gmail.com>
2024-10-24 11:20:38 +0900

    [design] 버튼 추가 및 margin 수정

--- components/templates/createKeyword/inquiry/InputInquiry.tsx
+++ components/templates/createKeyword/inquiry/InputInquiry.tsx
@@ -1,22 +1,28 @@
 'use client';
 
+import { Button } from '@/components/atoms/Button';
 import { KeywordInputRef } from '@/components/atoms/Inputs';
 
 export default function InputInquiry() {
   return (
-    <div className='flex flex-col gap-[24px]'>
-      <h1 className='font-extrabold text-2xl'>어떤 내용을 조회할까요?</h1>
-      <div className='flex flex-col items-center mt-[50px]'>
+    <div className='flex flex-col gap-[32px]'>
+      <h1 className='font-extrabold text-2xl mb-[50px]'>
+        어떤 내용을 조회할까요?
+      </h1>
+      <div className='flex flex-col items-center mt-[10px]'>
         <KeywordInputRef
           placeHolder='조회할 내용을 작성해주세요'
-          classNames='text-hanaPrimary'
+          className='text-hanaPrimary'
         />
       </div>
-      <div className='flex flex-col gap-6 text-center font-'>
+      <div className='mx-[10px]'>
+        <Button className='w-full'>완료</Button>
+      </div>
+      <div className='flex flex-col gap-5 text-center font-semibold text-[14px]'>
         <h1>거래 내역에 적히는 내용으로만 조회돼요</h1>
         <h1>
-          ex) 9월 급여, 10월 급여와 같이 매달 달라진다면 공통 단어인 “급여”를
-          작성해주세요
+          ex) 9월 급여, 10월 급여와 같이 매달 달라진다면 <br />
+          공통 단어인 “급여”를 작성해주세요
         </h1>
       </div>
     </div>
