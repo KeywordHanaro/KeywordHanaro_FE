49fed03c815ac176d9ee8e9335b3f8183648d713
insun-k <kis8294@gmail.com>
2024-10-24 14:11:20 +0900

    [refactor] keywordInputButton 컴포넌트 적용

--- components/templates/createKeyword/inquiry/InputInquiry.tsx
+++ components/templates/createKeyword/inquiry/InputInquiry.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { Button } from '@/components/atoms/Button';
-import { KeywordInputRef } from '@/components/atoms/Inputs';
+import KeywordInputButton from '../../InputButton';
 
 export default function InputInquiry() {
   return (
@@ -9,17 +8,9 @@ export default function InputInquiry() {
       <h1 className='font-extrabold text-2xl mb-[50px]'>
         어떤 내용을 조회할까요?
       </h1>
-      <div className='flex flex-col items-center mt-[10px]'>
-        <KeywordInputRef
-          placeHolder='조회할 내용을 작성해주세요'
-          className='text-hanaPrimary'
-        />
-      </div>
-      <div className='mx-[10px] flex items-center'>
-        <Button size='lg' isDisabled={true}>
-          완료
-        </Button>
-      </div>
+
+      <KeywordInputButton placeHolder='조회할 내용을 작성해주세요' />
+
       <div className='flex flex-col gap-5 text-center font-semibold text-[14px]'>
         <h1>거래 내역에 적히는 내용으로만 조회돼요</h1>
         <h1>
