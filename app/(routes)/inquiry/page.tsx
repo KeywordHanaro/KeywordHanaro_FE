55209ff4550fdd1bf04ea18fd7773676d44f768b
insun-k <kis8294@gmail.com>
2024-10-28 09:39:54 +0900

    [feat] use keyword > inquiry 페이지 생성

--- app/(routes)/inquiry/page.tsx
+++ app/(routes)/inquiry/page.tsx
@@ -1,3 +1,28 @@
+'use client';
+
+import Header from '@/components/atoms/Header';
+import { MicRef } from '@/components/atoms/Mic';
+import TransactionList from '@/components/templates/useKeyword/inquiry/TransactionList';
+import { useRouter } from 'next/navigation';
+
 export default function InquiryPage() {
-  return <div>InquiryPage</div>;
+  const router = useRouter();
+
+  const handleOnBack = () => {
+    router.push('/');
+  };
+
+  return (
+    <div className='flex flex-col h-full'>
+      <Header
+        text='키워드 내역 조회'
+        onBack={handleOnBack}
+        showActionButton={false}
+      />
+      <div className='flex flex-col flex-grow  overflow-y-scroll pt-[10px] px-5 pb-24 gap-2.5'>
+        <TransactionList />
+      </div>
+      <MicRef />
+    </div>
+  );
 }
