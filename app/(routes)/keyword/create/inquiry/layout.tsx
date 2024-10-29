28f386e4556fad47c7d783319f69283bf38fb18f
insun-k <kis8294@gmail.com>
2024-10-29 11:15:06 +0900

    step4 마이크 수정

--- app/(routes)/keyword/create/inquiry/layout.tsx
+++ app/(routes)/keyword/create/inquiry/layout.tsx
@@ -3,27 +3,19 @@
 import Header from '@/components/atoms/Header';
 import { MicRef } from '@/components/atoms/Mic';
 import { InquiryProvider } from '@/contexts/InquiryContext';
+import { usePathname } from 'next/navigation';
 // import { useRouter } from 'next/navigation';
 import { ReactNode } from 'react';
 
 export default function InquiryLayout({ children }: { children: ReactNode }) {
-  //   const router = useRouter();
-
-  //   const handleOnBack = () => {
-  //     if (step === 1) router.back();
-  //     else prevStep();
-  //   };
+  const pathname = usePathname();
 
   return (
     <InquiryProvider>
       <div className='h-screen relative'>
-        <Header
-          text='키워드 생성하기'
-          //   onBack={handleOnBack}
-          showActionButton={false}
-        />
+        <Header text='키워드 생성하기' showActionButton={false} />
         <div className='px-[20px] mt-[24px]'>{children}</div>
-        <MicRef />
+        {!pathname.includes('step4') && <MicRef />}
       </div>
     </InquiryProvider>
   );
