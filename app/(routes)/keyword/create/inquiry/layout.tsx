a538452ab7e4a1f91f4ade683e820c4a130b4887
ezenjun <ezenjun07@gmail.com>
2024-12-02 15:46:23 +0900

    [feat] : 조회 생성 STT 연동 1차

--- app/(routes)/keyword/create/inquiry/layout.tsx
+++ app/(routes)/keyword/create/inquiry/layout.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+// import { MicRef } from '@/components/atoms/Mic';
+import SpeechToText from '@/components/SpeechToText';
 import Header from '@/components/atoms/Header';
-import { MicRef } from '@/components/atoms/Mic';
 import { InquiryProvider } from '@/contexts/InquiryContext';
+import { VoiceInputProvider } from '@/contexts/VoiceContext';
 import { usePathname } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import { ReactNode } from 'react';
-
 
 export default function InquiryLayout({ children }: { children: ReactNode }) {
   const router = useRouter();
@@ -21,26 +22,28 @@ export default function InquiryLayout({ children }: { children: ReactNode }) {
   };
 
   return (
-    <InquiryProvider>
-      <div className='h-screen relative'>
-        {pathname.startsWith('/keyword/create/inquiry/step4') ? (
-          <Header
-            text='키워드 생성하기'
-            showActionButton={false}
-            showBackButton={false}
-            // onBack={handleOnBack}
-          />
-        ) : (
-          <Header
-            text='키워드 생성하기'
-            showActionButton={false}
-            onBack={handleOnBack}
-          />
-        )}
+    <VoiceInputProvider>
+      <InquiryProvider>
+        <div className='h-screen relative'>
+          {pathname.startsWith('/keyword/create/inquiry/step4') ? (
+            <Header
+              text='키워드 생성하기'
+              showActionButton={false}
+              showBackButton={false}
+              // onBack={handleOnBack}
+            />
+          ) : (
+            <Header
+              text='키워드 생성하기'
+              showActionButton={false}
+              onBack={handleOnBack}
+            />
+          )}
 
-        <div className='px-[20px] mt-[24px]'>{children}</div>
-        {!pathname.includes('step4') && <MicRef />}
-      </div>
-    </InquiryProvider>
+          <div className='px-[20px] mt-[24px]'>{children}</div>
+          {!pathname.includes('step4') && <SpeechToText />}
+        </div>
+      </InquiryProvider>
+    </VoiceInputProvider>
   );
 }
--- contexts/VoiceContext.tsx
+++ contexts/VoiceContext.tsx
@@ -4,66 +4,32 @@
 import {
   createContext,
   PropsWithChildren,
-  useContext,
-  useEffect,
+  useContext, // useEffect,
   useState,
 } from 'react';
 
-export type List = {
-  name: string;
-  link: string;
-};
+// export type List = {
+//   name: string;
+//   link: string;
+// };
 
 type VoiceInputProps = {
   result: string;
-  type: string;
   setResult: (data: string) => void;
-  setType: (data: string) => void;
   // setLists: (data: List[]) => void;
 };
 
 const VoiceInputContext = createContext<VoiceInputProps>({
   result: '',
-  type: '',
   setResult: () => {},
-  setType: () => {},
   // setLists: () => {},
 });
 
 export const VoiceInputProvider = ({ children }: PropsWithChildren) => {
   const [result, setResult] = useState<string>('');
-  const [type, setType] = useState<string>('');
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [socket, setSocket] = useState<WebSocket | null>(null);
-
-  //원하는 검색 결과 리스트로 지정
-  // const [lists, setLists] = useState<List[]>([]);
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   if (type == 'string') {
-  //     const list = lists.find((item) => item.name === result);
-  //     if (list) {
-  //       router.push(list.link);
-  //     }
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [result]);
-  useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8080/audio');
-    setSocket(ws);
-
-    ws.onmessage = (event) => {
-      setResult(event.data);
-    };
-
-    return () => {
-      ws.close();
-    };
-  }, []);
 
   return (
-    <VoiceInputContext.Provider value={{ result, type, setResult, setType }}>
+    <VoiceInputContext.Provider value={{ result, setResult }}>
       {children}
     </VoiceInputContext.Provider>
   );
