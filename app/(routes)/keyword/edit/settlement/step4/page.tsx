4f5c5fde8a81620f88ae26c4405bc9befd2bc401
Sung Yeob Jung <dev.sungyeob@gmail.com>
2024-12-26 18:50:58 +0900

    [fix] 정산 settelement

--- app/(routes)/keyword/edit/settlement/step4/page.tsx
+++ app/(routes)/keyword/edit/settlement/step4/page.tsx
@@ -2,13 +2,42 @@
 
 import KeywordInputButton from '@/components/templates/KeywordInputButton';
 import { useSettlementContext } from '@/contexts/SettlementContext';
+import { useKeywordApi } from '@/hooks/useKeyword/useKeyword';
 import { useRouter } from 'next/navigation';
 
 export default function SettlementStep4() {
-  const { formData, updateFormData } = useSettlementContext();
+  const { formData, updateFormData, id } = useSettlementContext();
   const router = useRouter();
+  const { updateKeyword } = useKeywordApi();
 
-  const nextStep = () => router.push('/keyword/edit/settlement/step5');
+  const nextStep = () => {
+    const groupMembers = formData.members.map((member) => ({
+      name: member.name,
+      tel: member.phoneNumber,
+    }));
+    const desc = `${formData.category === 'Settlement' ? '정산' : '회비'} > ${formData.checkEveryTime ? '' : formData.amount + ' >'} ${groupMembers.map(
+      (member) => member.name
+    )}`;
+    try {
+      console.log(formData);
+      updateKeyword(id, {
+        type: formData.category === 'Settlement' ? 'SETTLEMENT' : 'DUES',
+        name: formData.keywordName,
+        desc: desc,
+        account: {
+          id: formData.fromAccount.accountId,
+          accountNumber: formData.fromAccount.accountNumber,
+          accountName: formData.fromAccount.accountName,
+        },
+        checkEveryTime: formData.checkEveryTime,
+        amount: parseInt(formData.amount.replace(/,/g, ''), 10),
+        groupMember: JSON.stringify(groupMembers),
+      });
+    } catch {
+      throw new Error('Fetching Error');
+    }
+    router.push('/keyword/edit/settlement/step5');
+  };
   return (
     <KeywordInputButton
       placeHolder={'키워드 이름을 작성해주세요'}
