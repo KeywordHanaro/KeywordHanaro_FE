801a891c0229496687d0a6f3ce79ae0f6f0f0784
Sung Yeob Jung <dev.sungyeob@gmail.com>
2024-12-26 19:33:37 +0900

    [fix] next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/be/:path*',
+        destination: 'http://keyword.hanaro.topician.com/be/:path*',
+      },
+    ];
+  },
+};
 export default nextConfig;
