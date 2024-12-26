504a23c3b24ea30a0d131c4bb051a7b13bedf097
Sung Yeob Jung <dev.sungyeob@gmail.com>
2024-12-26 15:09:45 +0900

    [feat]Hanabank Loading

---
+++ components/atoms/LoadingDot.tsx
@@ -0,0 +1,11 @@
+export default function LoadingDot() {
+  return (
+    <>
+      <div className='w-full h-full flex justify-center items-center'>
+        <div className='dot-loading'>
+          <div className='middle-dot'></div>
+        </div>
+      </div>
+    </>
+  );
+}
