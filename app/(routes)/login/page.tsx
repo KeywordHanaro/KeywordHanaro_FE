3b46dfe4d6016465fadf12a9c888c6c7913c93dd
ezenjun <ezenjun07@gmail.com>
2024-12-28 16:43:36 +0900

    [fix] : 송금 오류 수정정

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -4,11 +4,11 @@ import { Button } from '@/components/atoms/Button';
 import { AuthInputRef } from '@/components/atoms/Inputs';
 import { useFormState, useFormStatus } from 'react-dom';
 import { FaLock } from 'react-icons/fa';
+// import { CiUser } from "react-icons/ci";
+import { FaUser } from 'react-icons/fa';
 // import { MdOutlineMailOutline } from 'react-icons/md';
 import { useRef } from 'react';
 import { authenticate } from '@/lib/actions';
-// import { CiUser } from "react-icons/ci";
-import { FaUser } from "react-icons/fa";
 
 export default function Login() {
   const [errorMsg, dispatchLogin] = useFormState(authenticate, undefined);
@@ -16,9 +16,11 @@ export default function Login() {
   const PwRef = useRef<HTMLInputElement>(null);
 
   return (
-    <div className='w-full flex justify-center h-5/6 my-background before:bg-opacity-20  overflow-hidden relative rounded-xl'>
+    <div className='flex flex-col justify-center items-center w-full h-full border-red-800'>
       <div className='flex justify-center items-center flex-col h-fit w-fit rounded-xl p-20 mt-20 bg-opacity-70 absolute z-100'>
-        <h1 className='mb-9'>Login</h1>
+        <h1 className='text-2xl font-semibold mb-9 text-hanaPrimary'>
+          키워드 하나로
+        </h1>
         <form action={dispatchLogin} className='space-y-3 flex flex-col gap-3'>
           <input type='hidden' name='redirectTo' value='/' />
           <AuthInputRef
